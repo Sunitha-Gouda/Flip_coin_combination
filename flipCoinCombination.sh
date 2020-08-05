0f21934a30bb2e47af5e269ef5b1204039b55927
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-05 19:23:35 +0530

    Singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,13 +1,35 @@
 #!/bin/bash -x
 
 HEAD=1;
-RANDOM_CHECK=$((RANDOM%2))
-
-if [ $RANDOM_CHECK -eq $HEAD ]
+FLIP_CHECK=$((RANDOM%2))
+NUM_OF_FLIPS=5
+if [ $FLIP_CHECK -eq $HEAD ]
 then
 	echo " Flip coin is HEADS "
 else
 	echo " Flip coin is TAILS "
 fi
 
+while [[ $NUMBER -le $NUM_OF_FLIPS ]]
+do
+	FLIP_CHECK=$((RANDOM%2))
 
+if [ $FLIP_CHECK -eq $HEAD ]
+then
+	COIN_FLIP="H"
+	HEAD_CNT=$(( $HEAD_CNT+1 ))
+else
+	COIN_FLIP="T"
+	TAIL_CNT=$(( $TAIL_CNT+1 ))
+fi
+
+	Singlet[$NUMBER]=$COIN_FLIP
+	NUMBER=$(( $NUMBER+1 ))
+	echo ${Singlet[@]}
+done
+
+	HEAD_PERCENT=$(echo | awk '{print '$HEAD_CNT/$NUM_OF_FLIPS*100'}')
+	TAIL_PERCENT=$(echo | awk '{print '$TAIL_CNT/$NUM_OF_FLIPS*100'}')
+
+ARRAY_SINGLET=($HEAD_PERCENT $TAIL_PERCENT)
+echo ${ARRAY_SINGLET[@]}
